e506cc1cc191f7a6f37cb6d7f381e7c380fd6eaf
Aatif Ali <aatifwarraich10@gmail.com>
2023-07-11 22:45:06 +0500

    infocard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,26 @@
-import React from "react";
-import DashboardLayout from "../../components/DashboardLayout";
-import PortfolioSecction from "./components/PortfolioSecction";
-import PriceSection from "./components/PriceSection";
 import { Grid, GridItem } from "@chakra-ui/react";
-import Transactions from "./components/Transaction";
+import DashboardLayout from "../../components/DashboardLayout";
+import PortfolioSecction from "../Dashboard/components/PortfolioSecction";
+import PriceSection from "../Dashboard/components/PriceSection";
+import Transactions from "../Dashboard/components/Transaction";
+import InfoCard from "../Dashboard/components/InfoCard";
 
-const Dashboard = ({ children }) => {
+const Dashboard = ({}) => {
   return (
-    <DashboardLayout title={Dashboard}>
+    <DashboardLayout title="Dashboard">
       <Grid
         gridTemplateColumns={{
-          base: "repeat(1,1fr)",
-          md: "repeat(2,1fr)",
+          base: "repeat(1, 1fr)",
+          xl: "repeat(2, 1fr)",
         }}
-        gap={6}
+        gap="6"
       >
-        <GridItem colSpan={2}>
+        <GridItem
+          colSpan={{
+            base: 1,
+            xl: 2,
+          }}
+        >
           <PortfolioSecction />
         </GridItem>
         <GridItem colSpan={1}>
@@ -23,6 +28,23 @@ const Dashboard = ({ children }) => {
         </GridItem>
         <GridItem colSpan={1}>
           <Transactions />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <InfoCard
+            imgUrl="/dot_bg.svg"
+            text=" Learn more about Loans – Keep your Bitcoin, access it’s value without
+          selling it"
+            tagText="Loan"
+            inverted={false}
+          />
+        </GridItem>
+        <GridItem colSpan={1}>
+          <InfoCard
+            inverted={true}
+            tagText="Contact"
+            imgUrl="/grid_bg.svg"
+            text="Learn more about our real estate, mortgage, and  corporate account services"
+          />
         </GridItem>
       </Grid>
     </DashboardLayout>
