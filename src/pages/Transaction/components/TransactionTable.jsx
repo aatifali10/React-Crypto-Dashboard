464536f90c811d8e5a5a17c928061f893fa97540
Aatif Ali <aatifwarraich10@gmail.com>
2023-07-12 13:53:19 +0500

    transaction complete

--- src/pages/Transaction/components/TransactionTable.jsx
+++ src/pages/Transaction/components/TransactionTable.jsx
@@ -14,8 +14,8 @@ const TransactionTable = () => {
   const tableData = [
     {
       id: "HD82NA2H",
-      date: "2023-06-20",
-      time: "07:00 AM",
+      date: "2023-07-12",
+      time: "01:29 PM",
       type: {
         name: "INR Deposit",
         tag: "E-Transfer",
@@ -25,8 +25,8 @@ const TransactionTable = () => {
     },
     {
       id: "HD82NA4H",
-      date: "2023-06-18",
-      time: "07:00 AM",
+      date: "2023-07-12",
+      time: "01:29 PM",
       type: {
         name: "INR Widthdraw",
         tag: "Wire Transfer",
@@ -36,8 +36,8 @@ const TransactionTable = () => {
     },
     {
       id: "HD82NA5H",
-      date: "2023-06-18",
-      time: "07:00 AM",
+      date: "2023-07-12",
+      time: "01:29 PM",
       type: {
         name: "Buy",
         tag: "BTC",
@@ -47,8 +47,8 @@ const TransactionTable = () => {
     },
     {
       id: "HD82NA6H",
-      date: "2023-06-18",
-      time: "07:00 AM",
+      date: "2023-07-12",
+      time: "01:29 PM",
       type: {
         name: "Sell",
         tag: "BTC",
@@ -58,8 +58,8 @@ const TransactionTable = () => {
     },
     {
       id: "HD82NA7H",
-      date: "2023-06-20",
-      time: "07:00 AM",
+      date: "2023-07-12",
+      time: "01:29 PM",
       type: {
         name: "BTC Deposit",
       },
@@ -68,8 +68,8 @@ const TransactionTable = () => {
     },
     {
       id: "HD82NA8H",
-      date: "2023-06-18",
-      time: "07:00 AM",
+      date: "2023-07-12",
+      time: "01:29 PM",
       type: {
         name: "BTC Widthdraw",
       },
